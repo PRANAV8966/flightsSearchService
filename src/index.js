6527266121e028f3a4c296a234c7be1ff50e1df4
PRANAV8966 <pranav.wan706@gmail.com>
2025-02-01 17:37:29 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const bodyParser = require('body-parser');
+
+const {PORT} = require('./config/serverConfig.js');
+
+const startFlightServer = async () => {
+
+    const app = express();
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
+
+    app.listen(PORT, () => {
+        console.log(`server started at ${PORT}`);
+    })
+
+}
+
+startFlightServer();

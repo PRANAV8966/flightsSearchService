5321e185e40bf7289d38b32d7435babbe837cc08
PRANAV8966 <pranav.wan706@gmail.com>
2025-02-02 13:48:05 +0530

    added CRUD functionalities to city repository

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cityRespository = require('./repository/cityRepository.js')
 
 const {PORT} = require('./config/serverConfig.js');
 
@@ -12,13 +11,7 @@ const startFlightServer = async () => {
     app.use(bodyParser.urlencoded({extended:true}));
 
     app.listen(PORT, async () => {
-        try{
             console.log(`server started at ${PORT}`);
-            const repo = new cityRespository();
-            repo.createCity({name:"delhi"});
-        } catch(error) {
-            throw {error};
-        }
     })
 }
 startFlightServer();
--- src/repository/cityRepository.js
+++ src/repository/cityRepository.js
@@ -2,22 +2,53 @@ const {city} = require('../models/index.js');
 
 
 class cityRespository {
+
+    // fucntion to create a new city entry  in DB
     async createCity({name}) {
         try{
             const City = await city.create({name});
             return City;
         } catch (error) {
+            console.log("something went wrong!!");
             throw {error};
         }
     }
-    async deleteCity({cityId}) {
+
+    // function to delete a existing city
+    async deleteCity(cityId) {
         try{
             await city.destroy({
                 where: {
-                    Id: cityId,
+                    Id: cityId
                 }
             });
         }catch (error) {
+            console.log("something went wrong!!");
+            throw {error};
+        }
+    }
+
+    // function to update an existing city 
+    async updateCity(cityId, {data}) {
+        try {
+            const City =  city.update(cityId, {
+                where: {
+                    id: cityId
+                }
+            })
+        } catch (error) {
+            console.log("something went wrong!!");
+            throw {error};
+        }
+    }
+    
+    // function to find a city
+    async getCity(cityId) {
+        try {
+            const City = city.findbypk(cityId);
+            return City;
+        } catch (error) {
+            console.log("something went wrong!!");
             throw {error};
         }
     }
